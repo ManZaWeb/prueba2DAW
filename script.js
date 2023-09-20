e1e970b23503176ac8d8dcfcde08472d9033dac4
Mario <maalnu93@hotmail.com>
2023-09-20 09:04:54 +0200

    Retocando funciones .js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-function cambiarColor() {
-    var nuevoColor = getRandomColor();
-    document.body.style.backgroundColor = nuevoColor;
-}
-
+//Funcion genera el color hexadecimal aleatorio.
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
+    const letters = '0123456789ABCDEF';
+    let color = '#';
     for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
+//Codigo mejorado
+//const color ="#"+Math.floor(Math.random()*16777215).toString(16);
+
+//Funcion para cambiar el nombre del body
+const cambiarColor = () => {
+    const nuevoColor=getRandomColor();
+    document.body.style.backgroundColor = nuevoColor;
+        console.log(nuevoColor);
+    
+    }
+
